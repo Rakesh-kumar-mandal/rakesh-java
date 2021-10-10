d41dbd5671be37b45a9dd73959304389eb93f74f
Rakesh <rakeshmandal484@gmail.com>
2021-10-10 20:29:14 +0530

    Initial commit

---
+++ Datatypes.java
@@ -0,0 +1,41 @@
+
+public class Datatypes {
+
+	public static void main(String args[]) {
+
+		byte my_age = 23;
+		short my_salary = 10000;
+		int pincode = 827013;
+		long my_phone = 978976897097L;
+
+		float percentage = 91.498988f;
+		double annualsalary = 27378263.37487438743d;
+
+				
+
+		boolean isIndian = true;
+		char section = 'B';
+
+		String my_name = "Rakesh 2378237 #%^@%#^#@^&";
+
+				
+
+
+		System.out.println(my_age);
+		System.out.println(my_salary);
+		System.out.println(pincode);
+		System.out.println(my_phone);
+		System.out.println(percentage);
+		System.out.println(annualsalary);
+		System.out.println(isIndian);
+		System.out.println(section);
+		System.out.println(my_name);
+
+
+		
+		
+
+			}
+		}
+
+		
---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld { // class
+
+	public static void main(String args[]) { // function/method
+		System.out.println("Hello world!");
+		System.out.println("Welcome Rakesh");
+		System.out.print("Java is a good langauge");
+	}
+}
